08107a32e2a82a0ab85dae6b20c63e35d10a40bd
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-06-17 20:19:00 +0530

    Debouncing is taking two arguments one function and delay

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const getData=()=>{
     // calls an API and gets data 
     console.log("Fetching Data ...",counter++);
 }
+
+const doSomeMagic = function(fn,d){
+    let timer;
+    return function(){
+        let context = this;
+        args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            getData.apply(context,arguments)
+            
+        }, d);
+    }
+}
+let betterFunction = doSomeMagic(getData,300); // Once we paused in typing then only API call should me made 
+/* Above function make API calls only if between  two keypresses time is more than 300 ms */
