cbabf19667e96df8e39e41da241dd49d1367bfbb
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-06-17 17:39:52 +0530

    Polyfil Impletation stsrted

--- bind.js
+++ bind.js
@@ -10,3 +10,22 @@ let name ={
  }
  let printMyName = printName.bind(name)
  printMyName();
+
+ /*
+ This functionality we have to write bind -> Implementation 
+ Every method in JS can access bind method 
+ How you will provide access to every JS method to your bind method let say -->mybind()
+ Function.prototype u can use for this
+ How can you give printName inside your implementation ?
+ when we call printMyName2 method printName should be called how u will do ? this variable  keyword --represent printName 
+ 
+ */
+Function.prototype.mybind = function(...args){
+    let obj = this
+    return function(){
+        obj.call(args[0]);
+    }
+}
+
+let printMyName2 = printName.mybind(name);
+printMyName2();
