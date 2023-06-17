f089a8d721fd8ed6cda8154360fa86ac76ac6ab1
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-06-17 15:52:47 +0530

    In call we have to pass individual argument after 1st argument which is refernce

--- call_apply_bind.js
+++ call_apply_bind.js
@@ -4,15 +4,15 @@ let name = {
     
 }
 
-let printFullName =  function(hometown){
-    console.log(this.firstname+ " "+this.lastname+" From "+ hometown);
+let printFullName =  function(hometown, State){
+    console.log(this.firstname+ " "+this.lastname+" From "+ hometown+" , "+State);
 }
-printFullName.call(name,"Forbesganj");
+printFullName.call(name,"Forbesganj","Bihar");
 
 let name2 ={
     firstname: "Sachin",
     lastname: "Tendulkar"
 }
 // function borrowing --call ()
-printFullName.call(name2,"Mumbai");
+printFullName.call(name2,"Mumbai","Maharastra");
 
