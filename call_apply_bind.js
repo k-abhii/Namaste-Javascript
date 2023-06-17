34ef4949a9cd2d40d7ba9e381d66ebc8442dc7e2
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-06-17 15:56:12 +0530

    Apply we have to pass argument in arraylist

--- call_apply_bind.js
+++ call_apply_bind.js
@@ -7,12 +7,15 @@ let name = {
 let printFullName =  function(hometown, State){
     console.log(this.firstname+ " "+this.lastname+" From "+ hometown+" , "+State);
 }
-printFullName.call(name,"Forbesganj","Bihar");
+printFullName.apply(name,["Forbesganj","Bihar"]);
 
 let name2 ={
     firstname: "Sachin",
     lastname: "Tendulkar"
 }
 // function borrowing --call ()
+// call method 1st argument refernce after that you can pass function argument individually in call method argument
+// In apply only difference is we have to pass argument in arraylist
+
 printFullName.call(name2,"Mumbai","Maharastra");
 
