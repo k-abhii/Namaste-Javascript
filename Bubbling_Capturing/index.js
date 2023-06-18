a91fc9999a580a655753266f57d8cbc0947465de
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-06-18 15:28:42 +0530

    Update index.js

--- Bubbling_Capturing/index.js
+++ Bubbling_Capturing/index.js
@@ -9,3 +9,15 @@ document.querySelector("#parent").addEventListener('click',() => {
 document.querySelector("#child").addEventListener('click',() => {
     console.log("Child Clicked");
 });
+=================================
+    document.querySelector("#grandparent").addEventListener('click',() => {
+    console.log("GrandParent Clicked");
+},false);
+
+document.querySelector("#parent").addEventListener('click',() => {
+    console.log("Parent Clicked");
+},false);
+
+document.querySelector("#child").addEventListener('click',() => {
+    console.log("Child Clicked");
+},false);
